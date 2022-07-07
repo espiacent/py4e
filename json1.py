9b3451f29717ebc71471dcd9877bbb55bc2c54e5
espiacent <ch.gripp@posteo.ch>
2022-07-07 17:46:35 +0200

    add

---
+++ json1.py
@@ -0,0 +1,12 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+
+list = []
+url = 'http://py4e-data.dr-chuck.net/comments_1557542.json'
+
+file = urllib.request.urlopen(url).read()
+
+data = json.loads(file)
+
+for person in data['comments']:
+    list.append(person['count']) 
---
+++ xml1.py
@@ -0,0 +1,21 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+
+
+list = []
+
+url = 'http://py4e-data.dr-chuck.net/comments_1557541.xml'
+
+file = urllib.request.urlopen(url).read()
+
+tree = ET.fromstring(file)
+
+results = tree.findall('comments/comment')
+data_len = len(results)
+
+for result in results:
+   number = float(result.find('count').text)
+   list.append(number)
+
+total = sum(list)
+print(total)
